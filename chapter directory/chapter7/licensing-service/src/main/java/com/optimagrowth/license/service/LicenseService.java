b500bbaf0dbc80354c239c014e7ca8a242f5710c
Taegu <gmdals45@gmail.com>
2023-08-27 17:11:45 +0900

    [2023/08/27][rootPath] update licensing config

--- chapter directory/chapter7/licensing-service/src/main/java/com/optimagrowth/license/service/LicenseService.java
+++ chapter directory/chapter7/licensing-service/src/main/java/com/optimagrowth/license/service/LicenseService.java
@@ -125,7 +125,8 @@ public class LicenseService {
 	public List<License> getLicensesByOrganization(String organizationId) throws TimeoutException {
 		logger.debug("getLicensesByOrganization Correlation id: {}",
 				UserContextHolder.getContext().getCorrelationId());
-		randomlyRunLong();
+		//randomlyRunLong();
+		sleep();
 		return licenseRepository.findByOrganizationId(organizationId);
 	}
 
