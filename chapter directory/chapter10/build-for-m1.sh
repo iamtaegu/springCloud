a1daf05d0ab864c0f4934e8399eb1a74327fba86
iamtaegu <gmdals45@gmail.com>
2024-03-19 16:18:36 +0900

    2024.03.19 ADD .vimrc

--- chapter directory/chapter10/build-for-m1.sh
+++ chapter directory/chapter10/build-for-m1.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-mvn clean package
+#mvn clean package
 docker build -t ostock/configserver:0.0.1-SNAPSHOT --build-arg JAR_FILE=target/configserver-0.0.1-SNAPSHOT.jar ./configserver
 docker build -t ostock/eurekaserver:0.0.1-SNAPSHOT --build-arg JAR_FILE=target/eurekaserver-0.0.1-SNAPSHOT.jar ./eurekaserver
 docker build -t ostock/gatewayserver:0.0.1-SNAPSHOT --build-arg JAR_FILE=target/gatewayserver-0.0.1-SNAPSHOT.jar ./gatewayserver
