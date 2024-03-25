0bb74bc3f98ae7224f6a911d3cf269e9d82ad549
iamtaegu <gmdals45@gmail.com>
2024-03-25 09:37:13 +0900

    2024.03.25 UPDATE comment

--- chapter directory/chapter11/gatewayserver/src/main/java/com/optimagrowth/gateway/filters/ResponseFilter.java
+++ chapter directory/chapter11/gatewayserver/src/main/java/com/optimagrowth/gateway/filters/ResponseFilter.java
@@ -12,24 +12,27 @@ import reactor.core.publisher.Mono;
 
 @Configuration
 public class ResponseFilter {
- 
+
     final Logger logger =LoggerFactory.getLogger(ResponseFilter.class);
-    
+
+    // 추적ID 및 스팬ID 정보에 접근하는 진입점
     @Autowired
-   Tracer tracer;
-    
+    Tracer tracer;
+
     @Autowired
-	FilterUtils filterUtils;
- 
+    FilterUtils filterUtils;
+
     @Bean
     public GlobalFilter postGlobalFilter() {
         return (exchange, chain) -> {
             return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-            	  String traceId = tracer.currentSpan().context().traceIdString();
-            	  logger.debug("Adding the correlation id to the outbound headers. {}", traceId);
-                  exchange.getResponse().getHeaders().add(FilterUtils.CORRELATION_ID, traceId);
-                  logger.debug("Completing outgoing request for {}.", exchange.getRequest().getURI());
-              }));
+                String traceId = tracer.currentSpan().context().traceIdString();
+                logger.debug("Adding the correlation id to the outbound headers. {}", traceId);
+                // 슬루스 추적ID의 응답 헤더 tmx-correlation-id에 스팬 추가
+                exchange.getResponse().getHeaders().add(FilterUtils.CORRELATION_ID, traceId);
+                logger.debug("Completing outgoing request for {}.", exchange.getRequest().getURI());
+            }));
         };
     }
 }
+
